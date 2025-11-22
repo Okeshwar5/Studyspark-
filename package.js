3e82dd47f2a68001847d390e22cb79ed565de1bd
Okeshwar5 <werokesh@gmail.com>
2025-11-23 01:16:59 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+{
+  "name": "studyspark",
+  "version": "1.0.0",
+  "main": "node_modules/expo/AppEntry.js",
+  "scripts": {
+    "start": "expo start",
+    "android": "expo run:android",
+    "build": "eas build -p android"
+  },
+  "dependencies": {
+    "@react-navigation/bottom-tabs": "^7.0.0",
+    "@react-navigation/native": "^7.0.0",
+    "@react-navigation/native-stack": "^7.0.0",
+    "@supabase/supabase-js": "^2.39.3",
+    "expo": "~51.0.0",
+    "expo-document-picker": "~12.0.0",
+    "expo-file-system": "~17.0.0",
+    "react": "18.2.0",
+    "react-native": "0.74.0",
+    "@react-native-async-storage/async-storage": "~2.0.0"
+  }
+}
